af4df109181a2cc4766a9c41803d8d9bda3e1ee6
andsandv <anders.olsen.sandvik@finn.no>
2014-08-30 19:18:36 +0200

    add NewRelic

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+require('newrelic');
 var express = require('express');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
+
+app.get('/', function(request, response){
+  response.send('Hello World');
+});
 
 app.get('/', function(request, response){
   response.send('Hello World');
