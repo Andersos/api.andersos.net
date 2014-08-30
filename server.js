4ed9bb77e13d4ba0d175c351b13706a0311433c9
andsandv <anders.olsen.sandvik@finn.no>
2014-08-30 23:07:06 +0200

    Add favicon

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 require('newrelic');
 var express = require('express');
+var favicon = require('serve-favicon');
 var app = express();
 
 app.set('port', (process.env.PORT || 3000));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
-app.get('/', function(request, response){
-  response.send('Hello World');
-});
-
-app.get('/', function(request, response){
-  response.send('Hello World');
+app.get('/', function(req, res){
+  res.send('Hello World');
 });
 
 var server = app.listen(app.get('port'), function() {
