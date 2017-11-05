d5b1d3f2f3762b55e0e78b6d63188546ba8fd015
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2017-11-05 22:11:22 +0100

    Update culture

--- test/api.test.js
+++ test/api.test.js
@@ -1,8 +1,13 @@
+const activity = require('../activity.json');
 const assert = require('assert');
+const blood = require('../blood.json');
 const boardgames = require('../boardgames.json');
 const books = require('../books.json');
+const culture = require('../culture.json');
+const playerlog = require('../playerlog.json');
+const podcasts = require('../podcasts.json');
 const series = require('../series.json');
-const activity = require('../activity.json');
+const trips = require('../trips.json');
 
 function isJSON(str) {
   try {
@@ -19,9 +24,15 @@ function isValidJSON(json) {
   return isJSON(JSON.stringify(json));
 }
 
-describe('The books api', () => {
+describe('The activity api', () => {
   it('is valid JSON', () => {
-    assert(isValidJSON(books));
+    assert(isValidJSON(activity));
+  });
+});
+
+describe('The blood api', () => {
+  it('is valid JSON', () => {
+    assert(isValidJSON(blood));
   });
 });
 
@@ -31,14 +42,38 @@ describe('The boardgames api', () => {
   });
 });
 
+describe('The books api', () => {
+  it('is valid JSON', () => {
+    assert(isValidJSON(books));
+  });
+});
+
+describe('The culture api', () => {
+  it('is valid JSON', () => {
+    assert(isValidJSON(culture));
+  });
+});
+
+describe('The playerlog api', () => {
+  it('is valid JSON', () => {
+    assert(isValidJSON(playerlog));
+  });
+});
+
+describe('The podcasts api', () => {
+  it('is valid JSON', () => {
+    assert(isValidJSON(podcasts));
+  });
+});
+
 describe('The series api', () => {
   it('is valid JSON', () => {
     assert(isValidJSON(series));
   });
 });
 
-describe('The activity api', () => {
+describe('The trips api', () => {
   it('is valid JSON', () => {
-    assert(isValidJSON(activity));
+    assert(isValidJSON(trips));
   });
 });
