74fd92b378bb9aacc884c41701099d9d89400859
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2019-02-09 20:37:03 +0100

    Add some new movies

---
+++ getMovieData.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const fetch = require('node-fetch');
+const apikey = require('./secrets');
+
+function w2f(movie) {
+  console.log(`You rated ${movie.Title} ${movie['Your Rating']} of 10.`);
+  fs.writeFileSync('newMovie.json', JSON.stringify(movie));
+}
+
+async function getMovieData(id, moreData) {
+  return fetch(`http://www.omdbapi.com/?i=${id}&apikey=${apikey}`)
+    .then(r => r.json())
+    .then(movie => Object.assign(movie, moreData, {
+      URL: `https://www.imdb.com/title/${movie.imdbID}`,
+    }));
+}
+
+async function writeMovieDataToFile(id, moreData) {
+  const result = await getMovieData(id, moreData);
+  w2f(result);
+}
+
+writeMovieDataToFile('tt7014006', {
+  'Your Rating': 6,
+  'Date Watched': '2019-02-03',
+  'Date Rated': '2019-02-09',
+  Source: 'Popcorn',
+});
