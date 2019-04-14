c8c73b8ced4595fb353381118b14fce46660e360
Anders Olsen Sandvik <Andersos@users.noreply.github.com>
2019-04-14 12:53:09 +0200

    First man

--- getMovieData.js
+++ getMovieData.js
@@ -20,9 +20,9 @@ async function writeMovieDataToFile(id, moreData) {
   w2f(result);
 }
 
-writeMovieDataToFile('tt4633694', {
-  'Your Rating': 6,
-  'Date Watched': '2019-03-24',
-  'Date Rated': '2019-03-24',
-  Source: 'Popcorn',
+writeMovieDataToFile('tt1213641', {
+  'Your Rating': 5,
+  'Date Watched': '2019-04-13',
+  'Date Rated': '2019-04-13',
+  Source: 'Get',
 });
