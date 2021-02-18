c11252427def11b6f5960e8d5680e6efae747275
Anders Olsen Sandvik <288977+Andersos@users.noreply.github.com>
2021-02-18 12:35:48 +0100

    Test for valid date

--- test/api.test.js
+++ test/api.test.js
@@ -1,13 +1,13 @@
-const assert = require('assert');
-const activity = require('../activity.json');
-const blood = require('../blood.json');
-const boardgames = require('../boardgames.json');
-const books = require('../books.json');
+const assert = require("assert");
+const activity = require("../activity.json");
+const blood = require("../blood.json");
+const boardgames = require("../boardgames.json");
+const books = require("../books.json");
 // const culture = require('../culture.json');
-const playerlog = require('../playerlog.json');
-const podcasts = require('../podcasts.json');
-const series = require('../series.json');
-const movies = require('../movies.json');
+const playerlog = require("../playerlog.json");
+const podcasts = require("../podcasts.json");
+const series = require("../series.json");
+const movies = require("../movies.json");
 // const trips = require('../trips.json');
 // const runs = require('../runs.json');
 
@@ -18,6 +18,16 @@ function ElementsHasProperty(elements, prop) {
       console.log(value);
     }
     return acc && hasProp;
+  }, true);
+}
+
+function validDate(elements) {
+  return elements.reduce((acc, value) => {
+    const hasValidDate = /\d{4}-\d{2}-\d{2}/.test(value.date);
+    if (!hasValidDate) {
+      console.log(value);
+    }
+    return acc && hasValidDate;
   }, true);
 }
 
@@ -36,119 +46,136 @@ function isValidJSON(json) {
   return isJSON(JSON.stringify(json));
 }
 
-describe('The activity api', () => {
-  it('is valid JSON', () => {
+describe("The activity api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(activity));
   });
 
-  describe('each record', () => {
-    it('has date', () => {
-      assert(ElementsHasProperty(activity, 'date'));
+  describe("each record", () => {
+    it("has date", () => {
+      assert(ElementsHasProperty(activity, "date"));
     });
 
-    it('has steps', () => {
-      assert(ElementsHasProperty(activity, 'steps'));
+    it("has valid date", () => {
+      assert(validDate(activity));
+    });
+
+    it("has steps", () => {
+      assert(ElementsHasProperty(activity, "steps"));
     });
   });
 });
 
-describe('The blood api', () => {
-  it('is valid JSON', () => {
+describe("The blood api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(blood));
   });
 
-  describe('each record', () => {
-    it('has date', () => {
-      assert(ElementsHasProperty(blood, 'date'));
+  describe("each record", () => {
+    it("has date", () => {
+      assert(ElementsHasProperty(blood, "date"));
     });
 
-    it('has location', () => {
-      assert(ElementsHasProperty(blood, 'location'));
+    it("has valid date", () => {
+      assert(validDate(blood));
     });
 
-    it('has HbF', () => {
-      assert(ElementsHasProperty(blood, 'HbF'));
+    it("has location", () => {
+      assert(ElementsHasProperty(blood, "location"));
     });
 
-    it('has gift', () => {
-      assert(ElementsHasProperty(blood, 'gift'));
+    it("has HbF", () => {
+      assert(ElementsHasProperty(blood, "HbF"));
+    });
+
+    it("has gift", () => {
+      assert(ElementsHasProperty(blood, "gift"));
     });
   });
 });
 
-describe('The boardgames api', () => {
-  it('is valid JSON', () => {
+describe("The boardgames api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(boardgames));
   });
 
-  describe('each record', () => {
-    it('has name', () => {
-      assert(ElementsHasProperty(boardgames, 'name'));
+  describe("each record", () => {
+    it("has name", () => {
+      assert(ElementsHasProperty(boardgames, "name"));
     });
 
-    it('has time', () => {
-      assert(ElementsHasProperty(boardgames, 'time'));
+    it("has time", () => {
+      assert(ElementsHasProperty(boardgames, "time"));
     });
 
-    it('has minplayers', () => {
-      assert(ElementsHasProperty(boardgames, 'minplayers'));
+    it("has minplayers", () => {
+      assert(ElementsHasProperty(boardgames, "minplayers"));
     });
 
-    it('has maxplayers', () => {
-      assert(ElementsHasProperty(boardgames, 'maxplayers'));
+    it("has maxplayers", () => {
+      assert(ElementsHasProperty(boardgames, "maxplayers"));
     });
 
-    it('has link', () => {
-      assert(ElementsHasProperty(boardgames, 'link'));
+    it("has link", () => {
+      assert(ElementsHasProperty(boardgames, "link"));
     });
 
-    it('has thumbnail', () => {
-      assert(ElementsHasProperty(boardgames, 'thumbnail'));
+    it("has thumbnail", () => {
+      assert(ElementsHasProperty(boardgames, "thumbnail"));
     });
   });
 });
 
-describe('The books api', () => {
-  it('is valid JSON', () => {
+describe("The books api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(books));
   });
 
-  describe('each record', () => {
-    it('has name', () => {
-      assert(ElementsHasProperty(books, 'name'));
+  describe("each record", () => {
+    it("has name", () => {
+      assert(ElementsHasProperty(books, "name"));
     });
   });
 });
 
-describe('The culture api', () => {
-  it.skip('is valid JSON', () => {
+describe("The culture api", () => {
+  it.skip("is valid JSON", () => {
     // TODO: Wait until culture api is updated
     // assert(isValidJSON(culture));
   });
 });
 
-describe('The playerlog', () => {
-  it('is valid JSON', () => {
+describe("The playerlog", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(playerlog));
   });
 
-  describe('each record', () => {
-    it('has players', () => {
-      assert(ElementsHasProperty(playerlog, 'players'));
+  describe("each record", () => {
+    it("has players", () => {
+      assert(ElementsHasProperty(playerlog, "players"));
     });
 
-    it('has date', () => {
-      assert(ElementsHasProperty(playerlog, 'date'));
+    it("has date", () => {
+      assert(ElementsHasProperty(playerlog, "date"));
     });
 
-    it('has game', () => {
-      assert(ElementsHasProperty(playerlog, 'game'));
+     it("has valid date", () => {
+       assert(validDate(playerlog));
+     });
+
+    it("has game", () => {
+      assert(ElementsHasProperty(playerlog, "game"));
     });
 
-    it.skip('has winners', () => {
+    it.skip("has winners", () => {
       const allHasWinners = playerlog.reduce((acc, value) => {
-        const hasWinners = Object.prototype.hasOwnProperty.call(value, 'winners');
-        const noStats = Object.prototype.hasOwnProperty.call(value, 'stats') && value.stats === false;
+        const hasWinners = Object.prototype.hasOwnProperty.call(
+          value,
+          "winners"
+        );
+        const noStats =
+          Object.prototype.hasOwnProperty.call(value, "stats") &&
+          value.stats === false;
         const countWinners = hasWinners || noStats;
         if (!countWinners) {
           console.log(value);
@@ -161,71 +188,71 @@ describe('The playerlog', () => {
   });
 });
 
-describe('The podcasts api', () => {
-  it('is valid JSON', () => {
+describe("The podcasts api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(podcasts));
   });
 
-  describe('each record', () => {
-    it('has name', () => {
-      assert(ElementsHasProperty(podcasts, 'name'));
+  describe("each record", () => {
+    it("has name", () => {
+      assert(ElementsHasProperty(podcasts, "name"));
     });
 
-    it('has last', () => {
-      assert(ElementsHasProperty(podcasts, 'last'));
+    it("has last", () => {
+      assert(ElementsHasProperty(podcasts, "last"));
     });
   });
 });
 
-describe('The movies api', () => {
-  it('is valid JSON', () => {
+describe("The movies api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(movies));
   });
 
-  describe('each record', () => {
-    it('has name', () => {
-      assert(ElementsHasProperty(movies, 'Title'));
+  describe("each record", () => {
+    it("has name", () => {
+      assert(ElementsHasProperty(movies, "Title"));
     });
 
-    it('has imdb id', () => {
-      assert(ElementsHasProperty(movies, 'Const'));
+    it("has imdb id", () => {
+      assert(ElementsHasProperty(movies, "Const"));
     });
 
-    it('has date', () => {
-      assert(ElementsHasProperty(movies, 'Date Rated'));
+    it("has date", () => {
+      assert(ElementsHasProperty(movies, "Date Rated"));
     });
 
-    it('has has rating', () => {
-      assert(ElementsHasProperty(movies, 'Your Rating'));
+    it("has has rating", () => {
+      assert(ElementsHasProperty(movies, "Your Rating"));
     });
   });
 });
 
-describe('The series api', () => {
-  it('is valid JSON', () => {
+describe("The series api", () => {
+  it("is valid JSON", () => {
     assert(isValidJSON(series));
   });
 
-  describe('each record', () => {
-    it('has name', () => {
-      assert(ElementsHasProperty(series, 'name'));
+  describe("each record", () => {
+    it("has name", () => {
+      assert(ElementsHasProperty(series, "name"));
     });
 
-    it('has lastWatched', () => {
-      assert(ElementsHasProperty(series, 'lastWatched'));
+    it("has lastWatched", () => {
+      assert(ElementsHasProperty(series, "lastWatched"));
     });
   });
 });
 
-describe('The trips api', () => {
-  it.skip('is valid JSON', () => {
+describe("The trips api", () => {
+  it.skip("is valid JSON", () => {
     // TODO: Wait until trips api is updated
     // assert(isValidJSON(trips));
   });
 });
 
-describe('The runs api', () => {
-  it.skip('is valid JSON', () => {
+describe("The runs api", () => {
+  it.skip("is valid JSON", () => {
     // TODO: Wait until runs api is updated
     // assert(isValidJSON(trips));
   });
